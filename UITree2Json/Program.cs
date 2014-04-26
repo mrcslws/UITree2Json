1c5d7a30910c8a9a33703cc3424868e253ca5aec
Marcus Lewis <marcus@outlook.com>
2014-04-26 11:08:19 -0700

    Add semi-functional "-subscribe2updates"

--- UITree2Json/Program.cs
+++ UITree2Json/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Windows.Automation;
 using Newtonsoft.Json;
 
@@ -23,98 +24,227 @@ namespace UITree2Json
         static object ElementAndDescendents2SerializableObject(
             AutomationElement rootElement,
             int maxDepth,
-            List<string> ignoreChildrenOfTheseClassNames)
+            List<string> ignoreChildrenOfTheseClassNames,
+            bool addStructureChangedEventHandler = false)
         {
-            Dictionary<string, object> properties = new Dictionary<string, object>
+            try
             {
-                {"name", rootElement.Current.Name},
-                {"className", rootElement.Current.ClassName},
-            };
-
-            if(!ignoreChildrenOfTheseClassNames.Any(
-                stopDiggingAfterThisClassName => rootElement.Current.ClassName == stopDiggingAfterThisClassName
-                ))
-            {
-                List<object> children = new List<object>();
-                if (maxDepth > 0)
+                if (addStructureChangedEventHandler)
                 {
-                    AutomationElement child = TreeWalker.ControlViewWalker.GetFirstChild(rootElement);
-                    while (child != null)
+                    Automation.AddStructureChangedEventHandler(rootElement, TreeScope.Descendants,
+                        (sender, e) =>
+                        {
+                            AutomationElement senderElement = (AutomationElement)sender;
+                            if(senderElement != null)
+                            {
+                                // Check if this event is coming from below a class that we're ignoring
+                                bool ignoreThisOne = false;
+                                try
+                                {
+                                    AutomationElement ancestor = TreeWalker.ControlViewWalker.GetParent(senderElement);
+                                    while (ancestor != null)
+                                    {
+                                        string cachedAncestorClassName = ancestor.Current.ClassName;
+                                        if (ignoreChildrenOfTheseClassNames.Any(
+                                            stopDiggingAfterThisClassName => cachedAncestorClassName == stopDiggingAfterThisClassName
+                                            ))
+                                        {
+                                            ignoreThisOne = true;
+                                            break;
+                                        }
+
+                                        ancestor = TreeWalker.ControlViewWalker.GetParent(ancestor);
+                                    }
+                                }
+                                catch (ElementNotAvailableException)
+                                {
+                                }
+
+                                if (!ignoreThisOne)
+                                {
+                                    SerializeToConsoleIfNotNull(
+                                        ElementAndDescendents2SerializableObject(senderElement, maxDepth, ignoreChildrenOfTheseClassNames)
+                                        );
+                                }
+                            }
+                        });
+                }
+
+                string cachedClassName = rootElement.Current.ClassName;
+                Dictionary<string, object> properties = new Dictionary<string, object>
+                {
+                    {"name", rootElement.Current.Name},
+                    {"className", cachedClassName},
+                    {"isOffscreen", rootElement.Current.IsOffscreen},
+                };
+
+                if (!ignoreChildrenOfTheseClassNames.Any(
+                    stopDiggingAfterThisClassName => cachedClassName == stopDiggingAfterThisClassName
+                    ))
+                {
+                    List<object> children = new List<object>();
+                    if (maxDepth > 0)
                     {
-                        children.Add(ElementAndDescendents2SerializableObject(child, maxDepth - 1, ignoreChildrenOfTheseClassNames));
-                        child = TreeWalker.ControlViewWalker.GetNextSibling(child);
+                        AutomationElement child = TreeWalker.ControlViewWalker.GetFirstChild(rootElement);
+                        while (child != null)
+                        {
+                            children.Add(ElementAndDescendents2SerializableObject(child, maxDepth - 1, ignoreChildrenOfTheseClassNames));
+                            child = TreeWalker.ControlViewWalker.GetNextSibling(child);
+                        }
+                    }
+
+                    if (children.Count > 0)
+                    {
+                        properties.Add("children", children);
                     }
                 }
 
-                if (children.Count > 0)
-                {
-                    properties.Add("children", children);
-                }
+                return properties;
             }
+            catch (ElementNotAvailableException)
+            {
+                // It's gone
+                return null;
+            }
+        }
 
-            return properties;
+        static void SerializeToConsoleIfNotNull(object obj)
+        {
+            if (obj != null)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(obj));
+            }
         }
 
         static void Main(string[] args)
         {
-            if(args.Any(arg => arg.Contains("?")))
+            if (args.Any(arg => arg.Contains("?")))
             {
                 Console.WriteLine("Usage:");
-                Console.WriteLine("UITree2Json.exe [-maxdepth <n>] [-ignorebeneath <classname>] [processname0] [processname1]");
+                Console.WriteLine("UITree2Json.exe [-maxdepth <n>] [-ignorebeneath <classname>] [-subscribe2updates] [processname0] [processname1]");
                 Console.WriteLine();
                 Console.WriteLine("If you list no processes, I'll print the entire UI tree. Hope that's what you wanted!");
+                Console.WriteLine("subscribe2updates is only available if you specify process names");
             }
             else if (args.Length > 0)
             {
-                int maxDepth = int.MaxValue;
-                List<string> ignoreChildrenOfTheseClassNames = new List<string>();
-
                 List<string> prunedArgs = new List<string>(args);
 
-                int maxDepthFlagIndex = prunedArgs.FindIndex(arg => arg.Equals("-maxdepth", StringComparison.OrdinalIgnoreCase));
-                if(maxDepthFlagIndex != -1)
+                ManualResetEvent stayRunningUntilThisHandleIsSignalled = null;
                 {
-                    maxDepth = int.Parse(args[maxDepthFlagIndex + 1]);
-                    prunedArgs.RemoveRange(maxDepthFlagIndex, 2);
+                    int subscribe2UpdatesSwitchIndex = prunedArgs.FindIndex(arg => arg.Equals("-subscribe2updates", StringComparison.OrdinalIgnoreCase));
+                    if (subscribe2UpdatesSwitchIndex != -1)
+                    {
+                        stayRunningUntilThisHandleIsSignalled = new ManualResetEvent(false);
+                        Console.CancelKeyPress += (sender, e) =>
+                        {
+                            stayRunningUntilThisHandleIsSignalled.Set();
+                        };
+
+                        prunedArgs.RemoveAt(subscribe2UpdatesSwitchIndex);
+                    }
                 }
 
+                int maxDepth = int.MaxValue;
+                {
+                    int maxDepthOptionIndex = prunedArgs.FindIndex(arg => arg.Equals("-maxdepth", StringComparison.OrdinalIgnoreCase));
+                    if (maxDepthOptionIndex != -1)
+                    {
+                        maxDepth = int.Parse(args[maxDepthOptionIndex + 1]);
+                        prunedArgs.RemoveRange(maxDepthOptionIndex, 2);
+                    }
+                }
+
+                List<string> ignoreChildrenOfTheseClassNames = new List<string>();
                 while (true)
                 {
-                    int ignoreChildrenFlagIndex = prunedArgs.FindIndex(arg => arg.Equals("-ignorebeneath", StringComparison.OrdinalIgnoreCase));
-                    if (ignoreChildrenFlagIndex == -1)
+                    int ignoreChildrenOptionIndex = prunedArgs.FindIndex(arg => arg.Equals("-ignorebeneath", StringComparison.OrdinalIgnoreCase));
+                    if (ignoreChildrenOptionIndex == -1)
                     {
                         break;
                     }
 
-                    ignoreChildrenOfTheseClassNames.Add(prunedArgs[ignoreChildrenFlagIndex + 1]);
-                    prunedArgs.RemoveRange(ignoreChildrenFlagIndex, 2);
+                    ignoreChildrenOfTheseClassNames.Add(prunedArgs[ignoreChildrenOptionIndex + 1]);
+                    prunedArgs.RemoveRange(ignoreChildrenOptionIndex, 2);
                 }
 
-                prunedArgs.Select(
-                    providedProcessName => Regex.Replace(providedProcessName, "\\.exe$", "", RegexOptions.IgnoreCase)
-                    ).SelectMany(
-                    processName => Process.GetProcessesByName(processName)
-                    ).SelectMany(
-                    process => process.Threads.Cast<ProcessThread>()
-                    ).ToList().SelectMany(
-                    thread => { 
-                        List<IntPtr> hwnds = new List<IntPtr>();
-                        WindowsInterop.EnumThreadWindows(thread.Id,
-                            (hWnd, lParam) => { hwnds.Add(hWnd); return true; },
-                            IntPtr.Zero);
-                        return hwnds;
-                    }).Select(
-                    hwnd => AutomationElement.FromHandle(hwnd)
-                    ).ToList().ForEach(
-                    element => Console.WriteLine(JsonConvert.SerializeObject(
-                        ElementAndDescendents2SerializableObject(element, maxDepth, ignoreChildrenOfTheseClassNames)))
+                var unrecognizedSwitches = prunedArgs.Where(
+                    arg => arg.StartsWith("-")
+                    );
+                if (unrecognizedSwitches.Count() > 0)
+                {
+                    unrecognizedSwitches.ToList().ForEach(
+                        arg => Console.WriteLine("Unrecognized command line option: {0}", arg)
                         );
+                }
+                else
+                {
+                    var processesWeCareAbout = prunedArgs.Select(
+                        providedProcessName => Regex.Replace(providedProcessName, "\\.exe$", "", RegexOptions.IgnoreCase)
+                        );
+
+                    if (stayRunningUntilThisHandleIsSignalled != null)
+                    {
+                        Automation.AddAutomationEventHandler(
+                            WindowPattern.WindowOpenedEvent,
+                            AutomationElement.RootElement,
+                            TreeScope.Descendants,
+                            (sender, e) =>
+                            {
+                                try
+                                {
+                                    AutomationElement sourceElement = (AutomationElement)sender;
+                                    Process p = Process.GetProcessById(sourceElement.Current.ProcessId);
+                                    if (sourceElement != null &&
+                                        processesWeCareAbout.Any(
+                                        processName => p.ProcessName.Equals(processName, StringComparison.OrdinalIgnoreCase)
+                                        ))
+                                    {
+                                        SerializeToConsoleIfNotNull(
+                                            ElementAndDescendents2SerializableObject(sourceElement, maxDepth,
+                                                ignoreChildrenOfTheseClassNames, addStructureChangedEventHandler: true)
+                                            );
+                                    }
+                                }
+                                catch (ElementNotAvailableException)
+                                {
+                                    // It might've already been destroyed
+                                }
+                            }
+                            );
+                    }
+
+                    processesWeCareAbout.SelectMany(
+                        processName => Process.GetProcessesByName(processName)
+                        ).SelectMany(
+                        process => process.Threads.Cast<ProcessThread>()
+                        ).ToList().SelectMany(
+                        thread =>
+                        {
+                            List<IntPtr> hwnds = new List<IntPtr>();
+                            WindowsInterop.EnumThreadWindows(thread.Id,
+                                (hWnd, lParam) => { hwnds.Add(hWnd); return true; },
+                                IntPtr.Zero);
+                            return hwnds;
+                        }).Select(
+                        hwnd => AutomationElement.FromHandle(hwnd)
+                        ).ToList().ForEach(
+                        element => SerializeToConsoleIfNotNull(
+                            ElementAndDescendents2SerializableObject(element, maxDepth, ignoreChildrenOfTheseClassNames,
+                                addStructureChangedEventHandler: stayRunningUntilThisHandleIsSignalled != null))
+                            );
+
+                    if (stayRunningUntilThisHandleIsSignalled != null)
+                    {
+                        stayRunningUntilThisHandleIsSignalled.WaitOne();
+                    }
+                }
             }
             else
             {
-                Console.WriteLine(JsonConvert.SerializeObject(
+                SerializeToConsoleIfNotNull(
                     ElementAndDescendents2SerializableObject(AutomationElement.RootElement, maxDepth: int.MaxValue, ignoreChildrenOfTheseClassNames: new List<string>())
-                ));
+                );
             }
         }
     }
